c35e267af394d55e87731d724c18ef18c8ca763b
Maddie <maddiedeming1@gmail.com>
2017-12-13 15:45:13 -0500

    Bug fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,42 @@
-
-
+//Variables
 var guesses = 6;
 var lettersGuessed = [];
-var alert = document.getElementById("alert");
-var primaryButton = document.getElementById("primaryButton");
-var guessCard = document.getElementById("guessCard");
-var wordCard = document.getElementById("wordCard");
-var letterCard = document.getElementById("letterCard");
-var guessesRemaining = document.getElementById("guessesRemaining");
-var lettersGuessedDOM = document.getElementById("lettersGuessed");
+var incorrectLetters = [];
+var correctLetters = [];
 var word = "BLANKET";
-var wordLength = word.length;
 var letterIndex = [];
-var lettersLeft = word.length;
+//Document Element Variables
+var pageAlert = document.getElementById("alert");
+var pageButton = document.getElementById("primaryButton");
+var pageGuessCard = document.getElementById("guessCard");
+var pageWordCard = document.getElementById("wordCard");
+var pageLetterCard = document.getElementById("letterCard");
+var pageGuessContent = document.getElementById("guessesRemaining");
+var pageLetterContent = document.getElementById("lettersGuessed");
+
 
 function resetGame(){
-    lettersLeft = word.length;
     guesses = 6;
-    lettersGuessed = [];    
-    alert.innerHTML = "";
-    alert.classList.add('invisible');
-    alert.classList.remove('alert-warning');
-    alert.classList.remove('alert-danger');
-    alert.classList.remove('alert-success');
-    primaryButton.innerHTML = "Play Again"
-    primaryButton.classList.remove('btn-success');
-    primaryButton.classList.add('btn-info');
-    guessCard.classList.remove('invisible');
-    wordCard.classList.remove('invisible');
-    letterCard.classList.remove('invisible');
-    guessesRemaining.innerHTML = guesses;
-    guessCard.classList.remove('bg-warning');
-    guessCard.classList.remove('bg-danger');
-    guessCard.classList.add('bg-secondary');
-    lettersGuessedDOM.innerHTML = lettersGuessed;
-    for (var i = 0; i < letterIndex.length; i++){  
-        var letterText = document.getElementById(letterIndex[i]);
-        var element = document.getElementsByTagName("span"), index;
+    lettersGuessed = []; 
+    pageAlert.innerHTML = "";
+    pageAlert.classList.add('invisible');
+    pageAlert.classList.remove('alert-warning');
+    pageAlert.classList.remove('alert-danger');
+    pageAlert.classList.remove('alert-success');
+    pageButton.innerHTML = "Play Again"
+    pageButton.classList.remove('btn-success');
+    pageButton.classList.add('btn-info');
+    pageGuessCard.classList.remove('invisible');
+    pageWordCard.classList.remove('invisible');
+    pageLetterCard.classList.remove('invisible');
+    pageGuessContent.innerHTML = guesses;
+    pageGuessCard.classList.remove('bg-warning');
+    pageGuessCard.classList.remove('bg-danger');
+    pageGuessCard.classList.add('bg-secondary');
+    pageLetterContent.innerHTML = incorrectLetters;
+    for (var li = 0; li < letterIndex.length; li++){  
+        var letterText = document.getElementById(letterIndex[li]);
+        var element = document.getElementsByTagName("span"), li;
         for (index = element.length - 1; index >= 0; index--) {
             element[index].parentNode.removeChild(element[index]);
         }
@@ -45,24 +45,24 @@ function resetGame(){
 };
 
 function newWord(){
-    for (var i = 0; i < wordLength; i++){
+    for (var w = 0; w < word.length; w++){
         var newLetter = document.createElement("span");
-            newLetter.classList.add('badge');
-            newLetter.classList.add('badge-dark');
-            newLetter.classList.add('m-1');
+        newLetter.classList.add('badge');
+        newLetter.classList.add('badge-dark');
+        newLetter.classList.add('m-1');
         var parent = document.getElementById("currentWord");
-            parent.appendChild(newLetter);
+        parent.appendChild(newLetter);
         var text = document.createElement("p");
-        var t = document.createTextNode(word[i]);
-            text.appendChild(t);
-            newLetter.appendChild(text);  
-            text.classList.add('invisible');
-            text.style.width = "25px";
-            text.setAttribute("ID", word.indexOf(word[i]));
+        var t = document.createTextNode(word[w]);
+        text.appendChild(t);
+        newLetter.appendChild(text);  
+        text.classList.add('invisible');
+        text.style.width = "25px";
+        text.setAttribute("ID", word.indexOf(word[w]));
     }
 };
 
-document.onkeyup = function(event){
+document.onkeydown = function(event){
         play();
 };
 
@@ -78,57 +78,81 @@ function play(){
 function validateEntry(input){
     var letter = input.toUpperCase();
     var duplicate = false;
-    if(duplicate == false){
-        var showLetter = ""
-        var correctGuess = false
-        for (var i = 0; i < wordLength; i++){
-            if(letter == word[i]){
-                letterIndex.push(word.indexOf(word[i]));
+    var showLetter = "";
+    var correctGuess = false;
+    for (var cl = 0; cl < correctLetters.length; cl++){
+        if(letter == correctLetters[cl]){
+            duplicate = true;
+        }
+    }
+    for (var il = 0; il < incorrectLetters.length; il++){
+        if(letter == incorrectLetters[il]){
+            duplicate = true;
+        }
+    }
+    if (duplicate == false){
+        for (var l = 0; l < word.length; l++){
+            if(letter == word[l]){
+                correctLetters.push(word[l]);
+                //lettersGuessed.push(word[l]);
+                letterIndex.push(word.indexOf(word[l]));
                 correctGuess = true
-                showLetter = document.getElementById(letterIndex[i]);
-                console.log(showLetter);
-                showLetter.classList.remove('invisible');
-                lettersLeft--;
+                for (var e = 0; e < letterIndex.length; e++){
+                    
+                    showLetter = document.getElementById(parseInt(letterIndex[e]));
+                    showLetter.classList.remove('invisible');
+                }
             }
         }
         if(correctGuess == false){
             guesses--;
-            lettersGuessed.push(letter);
-            lettersGuessedDOM.innerHTML = lettersGuessed;
+            incorrectLetters.push(letter);
+            //lettersGuessed.push(letter);
+            pageLetterContent.innerHTML = incorrectLetters;
+            failCheck(guesses);
+        }
+        else if(correctGuess == true){
+            successCheck();
         }
     }
+};
+
+function failCheck(guesses){
     if(guesses >= 2){
-        guessesRemaining.innerHTML = guesses;
+        pageGuessContent.innerHTML = guesses;
     }
     else if(guesses == 1){
-        guessesRemaining.innerHTML = guesses;
-        guessCard.classList.remove('bg-secondary');
-        guessCard.classList.add('bg-warning');
-        alert.classList.remove('invisible');
-        alert.classList.add('alert-warning');
+        pageGuessContent.innerHTML = guesses;
+        pageGuessCard.classList.remove('bg-secondary');
+        pageGuessCard.classList.add('bg-warning');
+        pageAlert.classList.remove('invisible');
+        pageAlert.classList.add('alert-warning');
         alert.innerHTML = "You only have 1 more guess left. Choose wisely!";
     }
     else {
         if(guesses == 0){
-            guessesRemaining.innerHTML = guesses;
-            guessCard.classList.remove('bg-warning');
-            guessCard.classList.add('bg-danger');
-            alert.classList.remove('alert-warning');
-            alert.classList.add('alert-danger');
-            lettersGuessed = [];
-            alert.innerHTML = "You lose! Select the Button or press any key to play again!";
+            pageGuessContent.innerHTML = guesses;
+            pageGuessCard.classList.remove('bg-warning');
+            pageGuessCard.classList.add('bg-danger');
+            pageAlert.classList.remove('alert-warning');
+            pageAlert.classList.add('alert-danger');
+            pageAlert.innerHTML = "You lose! Select the Button or press any key to play again!";
         }
         else if(guesses < 0){
             resetGame();
         }
     }
-    if(lettersLeft == 0){
-        alert.classList.remove('alert-warning');
-        alert.classList.remove('alert-danger');
-        alert.classList.add('alert-success');
-        alert.classList.remove('invisible');
-        lettersGuessed = [];
-        guesses = -1;
-        alert.innerHTML = "Congratulations! You won! Select the Button or press any key to play again!";
+};
+
+function successCheck(){
+    if(correctLetters.length == word.length){
+        pageAlert.classList.remove('alert-warning');
+        pageAlert.classList.remove('alert-danger');
+        pageAlert.classList.add('alert-success');
+        pageAlert.classList.remove('invisible');
+        pageAlert.innerHTML = "Congratulations! You won! Select the Button or press any key to play again!";
+        document.onkeydown = function(event){
+            resetGame();
+        };
     }
 };
